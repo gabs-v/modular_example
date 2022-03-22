f468ab2a70e7d4562d0570c8b7b484bdb708f9ec
gabs-v <gabyvazquez8297@gmail.com>
2022-03-21 22:51:33 -0700

    first commit

---
+++ arithmetic.py
@@ -0,0 +1,9 @@
+#Getting familiar with modules and packages! :) 
+
+def add(x,y):
+    return x + y
+def multiply(x,y):
+    return x * y
+def subtract(x,y):
+    return x - y
+
---
+++ calculations.py
@@ -0,0 +1,4 @@
+import arithmetic
+print(arithmetic.add(5, 8))
+print(arithmetic.multiply(10, 5))
+print(arithmetic.subtract(12, 6))
